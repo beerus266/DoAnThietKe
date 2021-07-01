abba7fb03eb3b3642ce4c4b49caf8b2d9266caf9
beerus266 <nhai0626@gmail.com>
2021-07-01 08:44:34 +0700

    update function in web add-in

--- My Add In React/src/taskpane/components/App.js
+++ My Add In React/src/taskpane/components/App.js
@@ -38,21 +38,18 @@ export default class App extends React.Component {
   click = async () => {   
     try {
       await Excel.run(async (context) => {
-        var sheet = context.workbook.worksheets.getActiveWorksheet();
-        // const range = context.workbook.getSelectedRange();
-        var range = sheet.getRange("B1");
-        // var addr = range.load("address");
+                 const range = context.workbook.getSelectedRange();
+                 // Read the range address
+                 range.load("address");
+                var sheet = context.workbook.worksheets.getActiveWorksheet();
 
-        range.select();
-
-        var sheet1 = context.workbook.worksheets.getItem("Sample");
-
-        var range1 = sheet1.getRange("C3");
-        range1.values = [[ 5 ]];
-        range1.format.autofitColumns();
-
-        await context.sync();
-        console.log(`The range address was ${range.address}.`);
+                 await context.sync();
+                 console.log(`The range address was ${range.address}.`);
+                 var addr = range.address.slice(range.address.indexOf("!") + 1, range.address.length);
+                 var val = this.calculateNextPosition('left', addr)
+                //  range.values = [[ val ]];
+                var range1 = sheet.getRange(val);
+                range1.select();
       });
     } catch (error) {
       console.error(error);
@@ -62,25 +59,18 @@ export default class App extends React.Component {
   upCell = async () => {
     try {
       await Excel.run(async (context) => {
-
-        // Get Address of Cell Selected
-        var rangeSel = context.workbook.getSelectedRange();
-        rangeSel.load("address");
-        var pos = rangeSel.address;
-
-        // Calculate next Cell
-        pos = myArray.split(/([0-9]+)/);
-        positionChar = myArray[0];
-        positionInt = myArray[1];
-        let nextPosition = String.fromCharCode(myArray[0].charCodeAt(0) ) + (parseInt(myArray[1]) - 1);
-        
-        // Move to next cell
-        var sheet = context.workbook.worksheets.getActiveWorksheet();
-        var range = sheet.getRange(nextPosition);
-        range.select();
+        const range = context.workbook.getSelectedRange();
+        // Read the range address
+        range.load("address");
+       var sheet = context.workbook.worksheets.getActiveWorksheet();
 
         await context.sync();
         console.log(`The range address was ${range.address}.`);
+        var addr = range.address.slice(range.address.indexOf("!") + 1, range.address.length);
+        var val = this.calculateNextPosition('up', addr)
+        //  range.values = [[ val ]];
+        var range1 = sheet.getRange(val);
+        range1.select();
       });
     } catch (error) {
       console.error(error);
@@ -90,23 +80,18 @@ export default class App extends React.Component {
   downCell = async () => {
     try {
       await Excel.run(async (context) => {
-
-        var rangeSel = context.workbook.getSelectedRange();
-        rangeSel.load("address");
-        var pos = rangeSel.address;
-
-        pos = myArray.split(/([0-9]+)/);
-        positionChar = myArray[0];
-        positionInt = myArray[1];
-        let nextPosition = String.fromCharCode(myArray[0].charCodeAt(0) ) + (parseInt(myArray[1]) + 1);
-        
-        var sheet = context.workbook.worksheets.getActiveWorksheet();
-        var range = sheet.getRange(nextPosition);
-
-        range.select();
+        const range = context.workbook.getSelectedRange();
+        // Read the range address
+        range.load("address");
+       var sheet = context.workbook.worksheets.getActiveWorksheet();
 
         await context.sync();
         console.log(`The range address was ${range.address}.`);
+        var addr = range.address.slice(range.address.indexOf("!") + 1, range.address.length);
+        var val = this.calculateNextPosition('down', addr)
+       //  range.values = [[ val ]];
+       var range1 = sheet.getRange(val);
+       range1.select();
 
       });
     } catch (error) {
@@ -117,23 +102,18 @@ export default class App extends React.Component {
   leftCell = async () => {
     try {
       await Excel.run(async (context) => {
-
-        var rangeSel = context.workbook.getSelectedRange();
-        rangeSel.load("address");
-        var pos = rangeSel.address;
-
-        pos = myArray.split(/([0-9]+)/);
-        positionChar = myArray[0];
-        positionInt = myArray[1];
-        let nextPosition = String.fromCharCode(myArray[0].charCodeAt(0) - 1) + (parseInt(myArray[1]));
-        
-        var sheet = context.workbook.worksheets.getActiveWorksheet();
-        var range = sheet.getRange(nextPosition);
-
-        range.select();
+        const range = context.workbook.getSelectedRange();
+        // Read the range address
+        range.load("address");
+       var sheet = context.workbook.worksheets.getActiveWorksheet();
 
         await context.sync();
         console.log(`The range address was ${range.address}.`);
+        var addr = range.address.slice(range.address.indexOf("!") + 1, range.address.length);
+        var val = this.calculateNextPosition('left', addr)
+       //  range.values = [[ val ]];
+       var range1 = sheet.getRange(val);
+       range1.select();
 
       });
     } catch (error) {
@@ -145,22 +125,18 @@ export default class App extends React.Component {
     try {
       await Excel.run(async (context) => {
 
-        var rangeSel = context.workbook.getSelectedRange();
-        rangeSel.load("address");
-        var pos = rangeSel.address;
-
-        pos = myArray.split(/([0-9]+)/);
-        positionChar = myArray[0];
-        positionInt = myArray[1];
-        let nextPosition = String.fromCharCode(myArray[0].charCodeAt(0) + 1 ) + (parseInt(myArray[1]));
-        
-        var sheet = context.workbook.worksheets.getActiveWorksheet();
-        var range = sheet.getRange(nextPosition);
-
-        range.select();
+        const range = context.workbook.getSelectedRange();
+        // Read the range address
+        range.load("address");
+       var sheet = context.workbook.worksheets.getActiveWorksheet();
 
         await context.sync();
         console.log(`The range address was ${range.address}.`);
+        var addr = range.address.slice(range.address.indexOf("!") + 1, range.address.length);
+        var val = this.calculateNextPosition('right', addr)
+       //  range.values = [[ val ]];
+       var range1 = sheet.getRange(val);
+       range1.select();
 
       });
     } catch (error) {
@@ -168,14 +144,19 @@ export default class App extends React.Component {
     }
   }
 
-  nextChar = (c) => {
-    return String.fromCharCode(c.charCodeAt(0) + 1);
+  calculateNextPosition = (direction, addr) => {
+    var myArray = addr.split(/([0-9]+)/);
+    // return positionChar;
+    if (direction == 'up'){
+      return String.fromCharCode(myArray[0].charCodeAt(0) ) + (parseInt(myArray[1]) - 1);
+    } else if (direction == 'down'){
+      return String.fromCharCode(myArray[0].charCodeAt(0) ) + (parseInt(myArray[1]) + 1);
+    }else if (direction == 'left'){
+      return String.fromCharCode(myArray[0].charCodeAt(0) - 1) + (parseInt(myArray[1]));
+    }else if (direction == 'right'){
+      return String.fromCharCode(myArray[0].charCodeAt(0) + 1 ) + (parseInt(myArray[1]));
+    }
   }
-  nextInt = (i) => {
-      return parseInt(i) + 1;
-  }
-
-  nextPosition
 
   render() {
     const { title, isOfficeInitialized } = this.props;
